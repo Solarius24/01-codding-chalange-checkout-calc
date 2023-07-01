6b233c4e2ce065b8e4ab2d148c1bf7b8986cdc3c
Pawel Karkocki <pawel.karkocki@gmail.com>
2023-07-01 17:18:44 +0100

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [itemsInPersonCart, setItemsInPersonCart] = useState(0);
+  const [lines, setLines] = useState([[10, 5, 2], [2], [2], [1], [5]]);
+
+  function addPersonToLine(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    let leastItemsAmoutn = 1e9;
+    let lineWithLeast: number[] | undefined = undefined;
+
+    for (let line of lines) {
+      const totalInLine = line.reduce((sum, value) => sum + value, 0);
+      if (totalInLine < leastItemsAmoutn) {
+        leastItemsAmoutn = totalInLine;
+        lineWithLeast = line;
+      }
+    }
+    if (!lineWithLeast) return;
+
+    setLines((preLines) =>
+      preLines.map((line) => {
+        if (line === lineWithLeast) {
+          return [...line, itemsInPersonCart];
+        } else {
+          return line;
+        }
+      })
+    );
+
+    setItemsInPersonCart(0);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLines((prevLines) => {
+        return prevLines.map((line) => {
+          return [line[0] - 1, ...line.slice(1)].filter(value => value >= 0)
+        });
+      });
+    }, 1000 / 2);
+    return clearInterval(interval);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addPersonToLine} className="form">
+        <label>ENTER AMOUNT</label>
+        <input
+          required
+          onChange={(e) => setItemsInPersonCart(e.currentTarget.valueAsNumber)}
+          type="number"
+          value={itemsInPersonCart}
+        ></input>
+        <button type="submit">ADD TO SHOPPING CART</button>
+      </form>
+      <div className="checkout_container">
+        {lines.map((line, idx) => (
+          <div key={idx}>
+            {line.map((numberOfItems) => (
+              <div>{numberOfItems}</div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
